c18b4a1952a8efcc51f3def3990761f484573e4b
Kyle McDonald <kyle@kylemcdonald.net>
2012-11-12 13:30:39 -0800

    refactoring

--- KinectFilter/src/ofApp.cpp
+++ KinectFilter/src/ofApp.cpp
@@ -9,32 +9,17 @@ void ofApp::setup() {
 	kinect.init(false, false);
 	kinect.setDepthClipping(500, 2000);
 	kinect.open();
-	
-	kinectFrameCount = 0;
-	hysteresis.resize(kinect.getWidth() * kinect.getHeight());
-	mask.allocate(kinect.getWidth(), kinect.getHeight(), OF_IMAGE_GRAYSCALE);
 }
 
 void ofApp::update() {
 	kinect.update();
 	if(kinect.isFrameNew()) {
-		unsigned char* kinectPixels = kinect.getDepthPixels();
-		unsigned char* maskPixels = mask.getPixels();
-		int n = kinect.getWidth() * kinect.getHeight();
-		for(int i = 0; i < n; i++) {
-			maskPixels[i] = hysteresis[i].update(kinectPixels[i] > 0, kinectFrameCount) ? 255 : 0;
-		}
-		mask.update();
-		
-		bitwise_and(kinect.getDepthPixelsRef(), mask, masked);
-		masked.update();
-		
-		kinectFrameCount++;
+		filter.update(kinect.getDepthPixelsRef());
 	}
 }
 
 void ofApp::draw() {
 	kinect.drawDepth(0, 0);
-	mask.draw(640, 0);
-	masked.draw(0, 480);
+	filter.getMask().draw(640, 0);
+	filter.getMasked().draw(0, 480);
 }
--- KinectFilter/src/ofApp.h
+++ KinectFilter/src/ofApp.h
@@ -12,8 +12,8 @@ protected:
 	static const unsigned int delay;
 public:
 	SimpleHysteresis()
-		:lastTime(0)
-		,curValue(false)
+	:lastTime(0)
+	,curValue(false)
 	{
 	}
 	const bool& update(const bool& value, const unsigned int& curTime) {
@@ -25,6 +25,40 @@ public:
 	}
 };
 
+class KinectFilter {
+protected:
+	unsigned int kinectFrameCount;
+	vector<SimpleHysteresis> hysteresis;
+	ofImage mask, masked;
+public:
+	KinectFilter()
+	:kinectFrameCount(0)
+	{
+	}
+	void update(ofPixels& pixels) {
+		ofxCv::imitate(mask, pixels);
+		unsigned char* kinectPixels = pixels.getPixels();
+		unsigned char* maskPixels = mask.getPixels();
+		int n = pixels.getWidth() * pixels.getHeight();
+		if(hysteresis.size() != n) {
+			hysteresis.resize(n);
+		}
+		for(int i = 0; i < n; i++) {
+			maskPixels[i] = hysteresis[i].update(kinectPixels[i] > 0, kinectFrameCount) ? 255 : 0;
+		}
+		mask.update();
+		ofxCv::bitwise_and(pixels, mask, masked);
+		masked.update();
+		kinectFrameCount++;
+	}
+	ofImage& getMask() {
+		return mask;
+	}
+	ofImage& getMasked() {
+		return masked;
+	}
+};
+
 class ofApp : public ofBaseApp {
 public:
 	void setup();
@@ -32,8 +66,5 @@ public:
 	void draw();
 	
 	ofxKinect kinect;
-	unsigned int kinectFrameCount;
-	vector<SimpleHysteresis> hysteresis;
-	ofImage mask;
-	ofImage masked;
+	KinectFilter filter;
 };
